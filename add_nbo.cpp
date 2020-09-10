607773850ffbff9a0a6fee587c0ec294d405e01c
rhfemrnt12 <rhfemrnt12@naver.com>
2020-09-10 19:32:50 +0900

    Update add_nbo.cpp

--- add_nbo.cpp
+++ add_nbo.cpp
@@ -14,15 +14,15 @@ int main(int argc, char *argv[]) {
     fp1=fopen(argv[1],"rb");
     fp2=fopen(argv[2],"rb");
 
-    char a[4], b[4];
+    unsigned char a[4], b[4];
     uint32_t a1, b1;
 
     fread(a, 1, 4, fp1);
     fread(b, 1, 4, fp2);
     
-    a1= (a[0]<<24) | (a[1]<<16) | (a[2]<<8) | (a[3]&0x000000ff);
-    b1= (b[0]<<24) | (b[1]<<16) | (b[2]<<8) | (b[3]&0x000000ff);
-    //a[3] and b[3] forms like 0xffffff__. why???
+    a1= (a[0]<<24) | (a[1]<<16) | (a[2]<<8) | (a[3]);
+    b1= (b[0]<<24) | (b[1]<<16) | (b[2]<<8) | (b[3]);
+    //a[3] and b[3] forms like 0xffffff__. why??? ==> signed=sign extension, unsigned=zero extension
 
     printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n",a1,a1,b1,b1,a1+b1,a1+b1);
 
